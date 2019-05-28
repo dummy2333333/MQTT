e554171a7de01dafd0de5b26c4907e020b22744c
Tyrael <lee.tyreal12138@outlook.com>
2019-05-29 01:43:11 +1000

    Delete test.java

--- src/test.java
+++
@@ -1,9 +0,0 @@
-import java.math.BigInteger;
-
-public class test {
-    public static void main(String[] args) {
-        for (int i = 0; i < 6; i++) {
-            System.out.println(i%3);
-        }
-    }
-}
